680dd4442ef991dc7fe048a47a80db3628c2c687
nextgenbite <iliusrahman@gmail.com>
2023-05-23 15:46:30 +0600

    route middleware added

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/pdf',[App\Http\Controllers\OrderController::class, 'viewPdf'])->middleware('auth')->name('view.pdf');
-// orders route
-Route::get('/dashboard',[App\Http\Controllers\OrderController::class, 'index'])->middleware('auth')->name('dashboard');
-Route::post('/order/store',[App\Http\Controllers\OrderController::class, 'store'])->name('order.store');
-Route::get('/order/view/{id}',[App\Http\Controllers\OrderController::class, 'show'])->name('order.view');
-Route::post('/order/delete',[App\Http\Controllers\OrderController::class, 'destroy'])->name('order.destroy');
+Route::group([ 'middleware'=>'auth'], function(){
+
+    // orders route
+    Route::get('/dashboard',[App\Http\Controllers\OrderController::class, 'index'])->middleware('auth')->name('dashboard');
+    Route::post('/order/store',[App\Http\Controllers\OrderController::class, 'store'])->name('order.store');
+    Route::get('/order/view/{id}',[App\Http\Controllers\OrderController::class, 'show'])->name('order.view');
+    Route::post('/order/delete',[App\Http\Controllers\OrderController::class, 'destroy'])->name('order.destroy');
+});
 require __DIR__.'/auth.php';
